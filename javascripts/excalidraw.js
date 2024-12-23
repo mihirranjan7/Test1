6688c16191c29c23aa4e42dffa1164c4e02d984b
mihirranjan7 <mihirranjan730@gmail.com>
2024-12-23 09:49:02 +0530

    Add files via upload

---
+++ javascripts/excalidraw.js
@@ -0,0 +1,35 @@
+import { apiInitializer } from "discourse/lib/api";
+import { ajax } from "discourse/lib/ajax";
+
+export default apiInitializer("0.11.1", api => {
+  api.onToolbarCreate(toolbar => {
+    toolbar.addButton({
+      id: "excalidraw-button",
+      icon: "paint-brush",
+      label: "Insert Drawing",
+      action: () => {
+        openExcalidrawModal();
+      },
+    });
+  });
+});
+
+function openExcalidrawModal() {
+  const modalHtml = `
+    <div id="excalidraw-container">
+      {{render "composer/excalidraw"}}
+    </div>
+  `;
+  
+  bootbox.dialog({
+    title: "Excalidraw",
+    message: modalHtml,
+    size: "large",
+  });
+
+  document.getElementById("insert-drawing").addEventListener("click", () => {
+    const composer = require("discourse/models/composer").default;
+    composer.appendText("[Excalidraw Drawing URL]");
+    bootbox.hideAll();
+  });
+}
